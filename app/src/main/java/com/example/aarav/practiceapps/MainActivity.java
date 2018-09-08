e1fff998856d18c89b9a27d55b1a63885d28e032
rawkush <ankushrawat950@gmail.com>
2018-09-08 21:46:17 +0530

    completed first practice app

--- app/src/main/java/com/example/aarav/practiceapps/MainActivity.java
+++ app/src/main/java/com/example/aarav/practiceapps/MainActivity.java
@@ -2,21 +2,40 @@ package com.example.aarav.practiceapps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    TextView textToChange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textToChange = findViewById(R.id.textToManupulate);
+
     }
 
 
+    public void buttonOnePressed(View variable){
+
+        textToChange.setText("Button One is Pressed");
+    }
 
 
+    public void buttonTwoPressed(View varible){
+        textToChange.setText("Button two is Pressed");
+
+    }
+
+
+    public void Reset(View var){
+
+        textToChange.setText("Nothing Pressed Yet");
+
+    }
 
 
 
