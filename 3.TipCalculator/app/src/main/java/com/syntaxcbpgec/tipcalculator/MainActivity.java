feb5460717e68b539f445075e996ba77c28080a1
Ahraar <ahraralam2012@gmail.com>
2018-10-02 09:58:25 +0530

    some things added

--- 3.TipCalculator/app/src/main/java/com/syntaxcbpgec/tipcalculator/MainActivity.java
+++ 3.TipCalculator/app/src/main/java/com/syntaxcbpgec/tipcalculator/MainActivity.java
@@ -2,8 +2,22 @@ package com.syntaxcbpgec.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvTipPercent, tvTipAmount, tvBillTotalAmount;
+    EditText etBillAmount;
+
+    float percentage = 0;
+    float tipTotal = 0;
+    float finalBillAmount = 0;
+    float totalBillAmount = 0;
+
+    float REGULAR_TIP_PERCENTAGE = 10;
+    float DEFAULT_TIP_PERCENTAGE = 15;
+    float EXCELLENT_TIP_PERCENTAGE = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
